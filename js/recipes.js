a126b8180ae9c788f439b415ab4d73d7577effbb
martynagolebowska <martynagx@gmail.com>
2016-12-01 10:51:30 +0100

    added recipes

---
+++ js/recipes.js
@@ -0,0 +1,142 @@
+var cakeIngrid =[];
+
+//chocolate cupcake
+cakeIngrid[0]=[
+    '1 1/3 cups all-purpose flour',
+    '1/4 teaspoon baking soda',
+    '2 teaspoons baking powder',
+    '3/4 cup unsweetened cocoa powder',
+    '1/8 teaspoon salt',
+    '3 tablespoons butter, softened',
+    '1 1/2 cups white sugar', 
+    '2 eggs',
+    '1 cup milk'
+];
+
+//lemon cupcake
+cakeIngrid[1]=[
+    '3 cups self-rising flour',
+    '1/2 teaspoon salt',
+    '1 cup unsalted butter, at room temperature',
+    '2 cups white sugar',
+    '4 eggs, at room temperature',
+    '2 tablespoons lemon zest',
+    '1 cup whole milk',
+    '2 1/2 tablespoons fresh lemon juice'  
+];
+
+//hazelnut cupcake
+cakeIngrid[2]=[
+    '1 cup hazelnuts, toasted and skinned',
+    '3/4 cup all-purpose flour',
+    '3/4 cup cake flour (not self-rising), sifted',
+    '1 1/2 teaspoons baking powder',
+    '1/2 cup (1 stick) unsalted butter, room temperature',
+    '1/4 cup granulated sugar',
+    '1/2 cup firmly packed dark-brown sugar',
+    '3/4 cup milk',
+    '4 large egg whites'
+];
+
+var cakeRecipe = [];
+
+//chocolate cupcake instruction
+cakeRecipe[0]=[
+    'Preheat oven to 350 degrees F (175 degrees C). Line a muffin pan with paper or foil liners. Sift together the flour, baking powder, baking soda, cocoa and salt. Set aside. In a large bowl, cream together the butter and sugar until light and fluffy. Add the eggs one at a time, beating well with each addition. Add the flour mixture alternately with the milk; beat well. Fill the muffin cups 3/4 full. Bake for 15 to 17 minutes in the preheated oven, or until a toothpick inserted into the cake comes out clean.'  
+];
+
+//lemon cupcake instruction
+cakeRecipe[1]=[
+    'Preheat oven to 375 degrees F (190 degrees C). Line 30 cupcake pan cups with paper liners. Sift the self-rising flour and salt together in a bowl. In another bowl, beat the unsalted butter and sugar with an electric mixer until light and fluffy. Beat in the eggs one at a time, beating each egg until incorporated before adding the next. Mix in lemon zest. Gently beat the flour mixture into the butter mixture, one third at a time, alternating with half the milk and half the lemon juice after each of the first 2 additions of flour. Beat until just combined; do not over mix.Fill the prepared cupcake liners with batter 3/4 full, and bake in the preheated oven until a toothpick inserted in the center comes out clean, about 17 minutes. Let the cupcakes cool in the pans for about 10 minutes before removing them to finish cooling on a rack.'
+];
+
+//hazelnut cupcake instruction
+cakeRecipe[1]=[
+    'Preheat oven to 350 degrees. Line standard muffin tins with paper liners. In a food processor, pulse hazelnuts until finely ground (do not overprocess, or the nuts will turn into a paste). Into a mixing bowl, sift together both flours, baking powder, and salt. Whisk in the ground nuts. With an electric mixer on medium-high speed, cream butter and both sugars until smooth. Reduce speed to low. Add flour mixture in three batches, alternating with two additions of milk, and beating well after each. In another mixing bowl, with electric mixer on medium speed, whisk the egg whites until soft peaks form. In two additions, gently fold the egg whites into the batter. Divide batter evenly among lined cups, filling each three-quarters full. Bake, rotating tins halfway through, until a cake tester inserted in centers comes out clean, about 25 minutes. Transfer tins to wire racks to cool completely before removing cupcakes.'
+];
+
+var fillingIngrid =[];
+
+//white chocolate
+fillingIngrid[0]=[
+    '4 ounces white chocolate, chopped',
+    '1/4 cup milk',
+    '4 ounces cream cheese',
+    '1/4 teaspoon pure vanilla extract'
+];
+
+//peanut butter
+fillingIngrid[1]=[
+    '1 cup creamy peanut butter',
+    '2/3 cup confectioners sugar',
+    '3 tablespoons of butter',
+    '1 teaspoon pure vanilla extract'  
+];
+
+//cherry jam
+fillingIngrid[2]=[
+    '4 cups pitted tart red cherries',
+    '1 cup white sugar',
+    '1/4 cup cornstarch',
+    '1 teaspoon pure vanilla extract'
+];
+
+var fillingRecipe = [];
+
+//white chocolate
+fillingRecipe[0]=
+    'In the top of a double boiler, combine the white chocolate and milk. Bring to a simmer and cook, stirring, until the chocolate is melted. Let cool to room temperature. In a bowl, beat the cream cheese, and vanilla. Gradually add the cooled chocolate and beat until smooth. Chill in the refrigerator for 2 hours before filling cupcakes. Use to fill the chocolate cupcakes as directed.'
+];
+
+//peanut butter
+fillingRecipe[1]=[
+    'In a medium bowl, beat the peanut butter with the remaining 3 tablespoons of butter until creamy. Sift the confectioners sugar into the bowl and beat until light and fluffy, about 2 minutes. Spoon all but 3 tablespoons of the peanut butter filling into a pastry bag fitted with a 1/4-inch star tip. Holding a cupcake in your hand, plunge the tip into the top of the cake, pushing it about 3/4 inch deep. Gently squeeze the pastry bag to fill the cupcake, withdrawing it slowly as you squeeze; you will feel the cupcake expand slightly as you fill it. Scrape any filling from the top of the cupcake and repeat until all of the cupcakes are filled.'
+];
+
+//cherry jam
+fillingRecipe[2]=[
+    'Place cherries into a saucepan over medium heat, and cover the pan; heat cherries until they release their juice and come to a simmer, 10 to 15 minutes. Stir often.In a bowl, whisk the sugar with cornstarch until smooth; pour the mixture and vanilla extract into the hot cherries and juice, and thoroughly combine. Return to low heat, bring to a simmer, and cook until the filling has thickened, about 2 minutes. Remove from heat and let it cool before filling the cupcake.'
+];
+
+var frostingIngrid =[];
+
+//chocolate frosting
+frostingIngrid[0]=[
+    '2 3/4 cups confectioners sugar',
+    '6 tablespoons unsweetened cocoa powder',
+    '6 tablespoons butter',
+    '5 tablespoons evaporated milk'
+];
+
+//meringue
+
+frostingIngrid[1]=[
+    '1/2 cup granulated sugar',
+    '1/4 teaspoon cream of tartar',
+    '2 egg whites',
+    '1 teaspoon vanilla extract'
+];
+
+frostingIngrid[2]=[
+    '3/4 cups (170g) unsalted butter, softened to room temperature',
+    '3 and 1/2 cups (420g) confectioners sugar',
+    '3 Tablespoons (45ml) heavy cream',
+    '1/2 cup (165g) thick raspberry preserves or jam'
+];
+
+var frostingRecipe = [];
+
+// chocolate frosting
+frostingRecipe[0]=[
+   'In a medium bowl, sift together the confectioners sugar and cocoa, and set aside. In a large bowl, cream butter until smooth, then gradually beat in sugar mixture alternately with evaporated milk. Blend in vanilla. Beat until light and fluffy. If necessary, adjust consistency with more milk or sugar. Pipe on chilled cupcakes'
+]
+
+// meringue frosting
+frostingRecipe[1]=[
+    'In a heatproof bowl set over simmering water, whisk together egg whites, sugar and cream of tartar, until egg whites are warm to the touch and sugar is dissolved. Mixture should be frothy. Remove from heat. Beat, using a hand mixer or stand mixer with whisk attachment, until glossy and stiff peaks form. Add vanilla in during last 30 seconds. Transfer to a large piping bag  fit with desired piping tip. Pipe on chilled cupcakes. Lightly char frosting using a kitchen torch.'
+];
+
+// raspberry frosting
+frostingRecipe[2]=[
+    'Beat softened butter on medium speed for about 3-4 minutes until completely smooth and creamy. Add the confectioners sugar, cream  with the mixer running. Increase to high speed and beat for 1 minute. Add the raspberry preserves and beat until thick and creamy, about 2 full minutes. Add more confectioners sugar if frosting is too thin, is separating (due to the fruity jam) or looks too wet. Pipe on chilled cupcakes'
+];
